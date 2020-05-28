ddd51f992cd51a7ed0789641879f91c422ef8872
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-28 16:43:03 -0400

    Created two input fields which update the local state when the user inputs information.

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('NoName');
+  const [name, setName] = useState('Blank');
   const [age, setAge] = useState('0');
-
-  const clickHandler = () => {
-    setName('Mason');
-    setPerson({name: 'Bob', age: 54})
-  }
 
   return (
     <View style={styles.container}>
-      <Text>Welcome to Trelliss!</Text>
-     
+      <Text style={styles.title}>Welcome to Trelliss!</Text>
+      <Text>Please enter your name:</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)}
+      />
+
+
+      <Text>Please enter your age:</Text>
+      <TextInput
+        keyboardType= 'numeric'       
+        style={styles.input}
+        placeholder='e.g. 99'
+        onChangeText={(val) => setAge(val)}
+      />
+
+
+
       <Text>Name: {name}, Age: {age}</Text>
     </View>
   );
@@ -26,5 +38,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+
+  title: {
+    fontSize: 40,
+  },
+
+  input: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    margin: 10,
+    borderColor: 'grey',
+    borderWidth: 1
+  },
 });
