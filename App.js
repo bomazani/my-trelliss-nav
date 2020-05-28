99d0f49484bb3ffe407c868e83f63ffeb3d0d457
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-28 15:08:13 -0400

    Created a function called 'clickHandler' which updates the state of 'name'. Created a new button that calls the clickHandler function when clicked.

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-// import 'useState'
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-// App is the root component of the app.
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 export default function App() {
-  // Add some state: 'name' is the actual piece of state, 'setName' is a function that allows us to update 'name' in the future.
-  // We set the initial value of 'name' to 'Bob'.
   const [name, setName] = useState('Bob');
+
+  const clickHandler = () => {
+    setName('Mason');
+  }
 
   return (
     <View style={styles.container}>
       <Text>Welcome to Trelliss!</Text>
-      {/* Use state to fill in name. */}
       <Text>My name is {name}.</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler}/>
+      </View>
     </View>
   );
 }
@@ -24,4 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20,
+    backgroundColor: 'grey',
+    color: 'green',
+  }
 });
