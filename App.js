0aa8e2a1ea7ec46dda875374c5b02436e6cae442
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-28 14:49:17 -0400

    Imported 'useState' from react. Added state for 'name' and applied it to some text.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+// import 'useState'
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 // App is the root component of the app.
 export default function App() {
+  // Add some state: 'name' is the actual piece of state, 'setName' is a function that allows us to update 'name' in the future.
+  // We set the initial value of 'name' to 'Bob'.
+  const [name, setName] = useState('Bob');
+
   return (
     <View style={styles.container}>
       <Text>Welcome to Trelliss!</Text>
+      {/* Use state to fill in name. */}
+      <Text>My name is {name}.</Text>
     </View>
   );
 }
