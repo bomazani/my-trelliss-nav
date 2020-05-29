544064ceb2b2d344caf1ac540c7fff7aaacf7821
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-29 18:14:03 -0400

    Adjusted code to establish base for todo app.

--- App.js
+++ App.js
@@ -1,60 +1,18 @@
-
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'Bob', id: '1'},
-    { name: 'Yoshi', id: '2'},
-    { name: 'Mario', id: '3'},
-    { name: 'Luigi', id: '4'},
-    { name: 'Peach', id: '5'},
-    { name: 'Toad', id: '6'},
-    { name: 'Bowser', id: '7'},
-  ]);
-
-  const pressHandler = (id) => {
-    // console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
-
   return (
     <View style={styles.container}>
-      <Text>Trelliss Touchable Components Page!</Text>
-
-      <FlatList
-        numColumns={ 2 }
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          // TouchableOpacity can have its own style prop.
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>
-              {item.name}
-            </Text>
-          </TouchableOpacity>
-        )}
-      />
-
+      <Text>Trelliss ToDo Page!</Text>
     </View> 
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
   },
-  item: {
-    marginTop: 35,
-    marginHorizontal: 10,
-    padding: 30,
-    backgroundColor: 'lightblue',
-    fontSize: 30,
-  }
+  
 });
