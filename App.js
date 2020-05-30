43364f7bba6e91f2c321c25074141af231ecf704
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-29 21:07:45 -0400

    Very basic list setup using FlatList.

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Item 1', key: '1' },
+    { text: 'Item 2', key: '2' },
+    { text: 'Item 3', key: '3' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Trelliss ToDo Page!</Text>
+      {/* header */}
+      {/* <Text style={styles.title}>Trelliss ToDo Page!</Text> */}
+
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View> 
   );
 }
