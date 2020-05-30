4ff2d145371b5d2708507a0b955f11a0a7116407
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-30 17:08:49 -0400

    Created an 'addTodo.js' component and imported into App.js. Input field displays, but new text does not yet.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
-
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -21,7 +21,7 @@ export default function App() {
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo />
         <View style={styles.list}>
           <FlatList
             data={todos}
