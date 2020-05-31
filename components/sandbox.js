aab4a56ed8a31b1db601b0d2b342aaeafce5ee4b
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-31 18:07:40 -0400

    Inside sandbox.js, created 4 Text components inside the View component. The Text components each have their own style and text.

--- components/sandbox.js
+++ components/sandbox.js
@@ -3,10 +3,36 @@ import {StyleSheet, Text, View } from 'react-native';
 
 export default function Sandbox() {
     return (
-        <Text>sandbox</Text>
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>one</Text>
+            <Text style={styles.boxTwo}>two</Text>
+            <Text style={styles.boxThree}>three</Text>
+            <Text style={styles.boxFour}>four</Text>            
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        // Views in react-native have flexbox built-in
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: 'blue',
+    },
 
+    boxOne: {
+
+    },
+
+    boxTwo: {
+
+    },
+
+    boxThree: {
+
+    },
+
+    boxFour: {
+
+    },
 });
