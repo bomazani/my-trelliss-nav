cd20867fda3fab23b3f47e2d81f94f18d9846a4a
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-31 10:29:45 -0400

    Deleted some commented code.

--- App.js
+++ App.js
@@ -21,10 +21,7 @@ export default function App() {
 
     if(text.length > 3){
       setTodos((prevTodos) => {
-        // Return a new array that includes the old array (...prevTodos)
-        // plus a new object with the new text and a unique id.
         return [
-          // Math.random() is not the best option, possible dupe id#s.
           { text: text, key: Math.random().toString() },
           ...prevTodos
         ];
@@ -43,14 +40,11 @@ export default function App() {
       <View style={styles.container}>
         <Header />
         <View style={styles.content}>
-          {/* passing submitHandler as a prop to AddTodo */}
           <AddTodo submitHandler={submitHandler} />
           <View style={styles.list}>
             <FlatList
               data={todos}
               renderItem={({ item }) => (
-                // item and presshandler are props passed into TodoItem
-                // so they can be used in todoItems.js
                 < TodoItem item={item} pressHandler={pressHandler} />
               )}
             />
