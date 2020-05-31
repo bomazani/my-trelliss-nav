aed28424b2079180d72b85bd3ce465bf88645384
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-31 18:33:13 -0400

    In App.js, commented out the Sandbox component and uncommented the original components, so we can apply flexbox styling to them.

--- App.js
+++ App.js
@@ -35,26 +35,26 @@ export default function App() {
   }
 
   return (
-    <Sandbox />
-    // <TouchableWithoutFeedback onPress={() => {
-    //   Keyboard.dismiss();
-    //   console.log('dismissed keyboard');
-    // }}>
-    //   <View style={styles.container}>
-    //     <Header />
-    //     <View style={styles.content}>
-    //       <AddTodo submitHandler={submitHandler} />
-    //       <View style={styles.list}>
-    //         <FlatList
-    //           data={todos}
-    //           renderItem={({ item }) => (
-    //             < TodoItem item={item} pressHandler={pressHandler} />
-    //           )}
-    //         />
-    //       </View>
-    //     </View>
-    //   </View> 
-    // </TouchableWithoutFeedback>
+    // <Sandbox />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                < TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View> 
+    </TouchableWithoutFeedback>
   );
 }
 
