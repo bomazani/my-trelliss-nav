8dccb8de6ee2c5fd5a23206be7b64e9752c14e2c
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-31 18:31:18 -0400

    Added flexbox styling and comments to the various components.

--- components/sandbox.js
+++ components/sandbox.js
@@ -15,28 +15,41 @@ export default function Sandbox() {
 const styles = StyleSheet.create({
     container: {
         // Views in react-native have flexbox built-in
-        flex: 1,
+        // Default is flexDirection 'column'
+        flexDirection: 'row',
+        // flex: determines how much of the available space is used.
+        flex: .25,
+        // justifyContent: determines how the items should be spread out on the main axis.
+        // default is : 'flex-start'
+        justifyContent: 'space-around',
+        // alignItems: determines how the items should be spread out on the cross axis.
+        // default is : 'flex-start'
+        alignItems: 'center',
         paddingTop: 40,
         backgroundColor: 'blue',
     },
 
     boxOne: {
+        flex: 1,
         backgroundColor: 'violet',
         padding: 10,
     },
 
     boxTwo: {
+        flex: 1,
         backgroundColor: 'gold',
-        padding: 10,
+        padding: 20,
     },
 
     boxThree: {
+        flex: 1,
         backgroundColor: 'coral',
-        padding: 10,
+        padding: 30,
     },
 
     boxFour: {
+        flex: 1,
         backgroundColor: 'skyblue',
-        padding: 10,
+        padding: 40,
     },
 });
