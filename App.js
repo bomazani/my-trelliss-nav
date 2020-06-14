206e5d1c14c3c32ef108fa12f33f9b2f3ce1722e
Bob Hunt <bob.hunt.email@gmail.com>
2020-06-14 13:48:31 -0400

    Added todoScreen. Upgraded logkitty to 0.7.1. Modified App.js.

--- App.js
+++ App.js
@@ -1,80 +1,28 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoItem';
-import AddTodo from './components/addTodo';
-import Sandbox from './components/sandbox';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  const [todos, setTodos] = useState([
-    { text: 'Item 1', key: '1' },
-    { text: 'Item 2', key: '2' },
-    { text: 'Item 3', key: '3' },
-  ]);
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key !== key)
-    })
-  }
-
-  const submitHandler = (text) => {
-
-    if(text.length > 3){
-      setTodos((prevTodos) => {
-        return [
-          { text: text, key: Math.random().toString() },
-          ...prevTodos
-        ];
-      });
-    } else {
-      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
-        {text: 'Understood', onPress: () => console.log('alert closed')}
-      ])
-    }
-  }
-
+function HomeScreen() {
   return (
-    // <Sandbox />
-    <TouchableWithoutFeedback onPress={() => {
-      Keyboard.dismiss();
-      console.log('dismissed keyboard');
-    }}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) => (
-                < TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-      </View> 
-    </TouchableWithoutFeedback>
+    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        HomeScreen
+      </Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+const Stack = createStackNavigator();
 
-  content: {
-    flex: 1,
-    padding: 40,
-    backgroundColor: 'beige',
-  },
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' compontent={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-  list: {
-    flex: 1,
-    marginTop: 40,
-    marginLeft: 20,
-  },
-
-  
-});
+export default App;
